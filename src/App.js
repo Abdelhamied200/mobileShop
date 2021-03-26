3bfc3fa8dc6e449f875d9fc5ef539e2c960364a4
Abdelhamied Mostafa <Abdelhamied200@gmail.com>
2021-03-27 00:32:16 +0200

    mocking up home and add pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Route, Switch } from "react-router";
 import "./App.scss";
+import Add from "./pages/add";
+import Home from "./pages/home";
 
 function App() {
-  return <div className="App">hello app</div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/add" component={Add} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/add/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Add = (props) => {
+  return (
+    <div className="page">
+      <div className="add">
+        <p>i am add page</p>
+      </div>
+    </div>
+  );
+};
+
+export default Add;
