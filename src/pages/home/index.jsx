8d7fefebed67a0e8db56d9a922e0cda2d358ecc8
Abdelhamied Mostafa <Abdelhamied200@gmail.com>
2021-03-27 02:10:02 +0200

    test search form

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "../../components/navbar";
+import SearchForm from "../../containers/searchForm";
 
 const Home = (props) => {
   return (
     <div className="page">
       <div className="home">
         <Navbar />
-        <p>i am home page</p>
+        <SearchForm />
       </div>
     </div>
   );
